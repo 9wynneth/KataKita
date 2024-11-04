3e2dc5fabcff56752e49bd46fa2393a1a26bc47c
9wynneth <gwynnethgwen10@gmail.com>
2024-11-04 10:59:52 +0700

    Temporary commit

---
+++ KataKita/View/VoiceSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  VoiceSettingsView.swift
+//  KataKita
+//
+//  Created by Gwynneth Isviandhy on 04/11/24.
+//
+
+import SwiftUI
+
+struct VoiceSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VoiceSettingsView()
+}
