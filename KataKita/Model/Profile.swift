8762119a68416f95ec046fe41aa0979a4c2a3304
Alvito Dwi Reza <alvito0x56@gmail.com>
2024-11-05 09:18:31 +0700

    Add new viewmodel
    feat: PECSViewModel and Profile Model

---
+++ KataKita/Model/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  KataKita
+//
+//  Created by Alvito Dwi Reza on 05/11/24.
+//
+
+import Foundation
+
+class Profile: Codable {
+    var name: String
+    var gender: Bool    // 0 for boy, 1 for gal
+    //TODO: CARI VOICE (Menunggu gwen)
+}
